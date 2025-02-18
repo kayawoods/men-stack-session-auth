5dc5c371bd03e5ca5619c2ea6da9ff09ce599685
Kaya Woods <kayanevada@gmail.com>
2025-02-18 11:51:08 -0800

    complete sign out functionality

--- controllers/auth.js
+++ controllers/auth.js
@@ -10,28 +10,56 @@ const User = require("../models/user.js");
 
 router.get("/sign-up", (req, res) => {
     res.render("auth/sign-up.ejs");
-  });
+});
 
-  router.post("/sign-up", async (req, res) => {
+router.post("/sign-up", async (req, res) => {
     const userInDatabase = await User.findOne({ username: req.body.username });
 
-if (userInDatabase) {
-  return res.send("Username already taken.");
-}
-if (req.body.password !== req.body.confirmPassword) {
-    return res.send("Password and Confirm Password must match");
-  }
-  const hashedPassword = bcrypt.hashSync(req.body.password, 10);
-req.body.password = hashedPassword;
+    if (userInDatabase) {
+        return res.send("Username already taken.");
+    }
+    if (req.body.password !== req.body.confirmPassword) {
+        return res.send("Password and Confirm Password must match");
+    }
+    const hashedPassword = bcrypt.hashSync(req.body.password, 10);
+    req.body.password = hashedPassword;
 
 
 
-const user = await User.create(req.body);
-res.send(`Thanks for signing up ${user.username}`);
+    const user = await User.create(req.body);
+    res.send(`Thanks for signing up ${user.username}`);
 
-  
-   
+
+});
+
+router.get("/sign-in", (req, res) => {
+    res.render("auth/sign-in.ejs");
+});
+
+router.post("/sign-in", async (req, res) => {
+    const userInDatabase = await User.findOne({ username: req.body.username });
+    if (!userInDatabase) {
+        return res.send("Login failed. Please try again.");
+    }
+    const validPassword = bcrypt.compareSync(
+        req.body.password,
+        userInDatabase.password
+    );
+    if (!validPassword) {
+        return res.send("Login failed. Please try again.");
+    }
+    req.session.user = {
+        username: userInDatabase.username,
+      };
+
+      res.redirect("/");
+
+});
+
+router.get("/sign-out", (req, res) => {
+    req.session.destroy();
+    res.redirect("/");
   });
-  
+
 
 module.exports = router;
--- server.js
+++ server.js
@@ -6,10 +6,13 @@ const app = express();
 const mongoose = require("mongoose");
 const methodOverride = require("method-override");
 const morgan = require("morgan");
+const session = require('express-session');
 
 // Set the port from environment variable or default to 3000
 const port = process.env.PORT ? process.env.PORT : "3000";
 const authController = require("./controllers/auth.js");
+
+
 
 
 mongoose.connect(process.env.MONGODB_URI);
@@ -25,12 +28,24 @@ app.use(methodOverride("_method"));
 // Morgan for logging HTTP requests
 app.use(morgan('dev'));
 
+app.use(
+    session({
+      secret: process.env.SESSION_SECRET,
+      resave: false,
+      saveUninitialized: true,
+    })
+  );
+
+
 app.use("/auth", authController);
 
 
-app.get("/", async (req, res) => {
-    res.render("index.ejs");
+app.get("/", (req, res) => {
+    res.render("index.ejs", {
+      user: req.session.user,
+    });
   });
+  
 
 app.listen(port, () => {
   console.log(`The express app is ready on port ${port}!`);
